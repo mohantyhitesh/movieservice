70ce1d9dcd7075a70ff220eaff3aecbac510ea19
mohantyhitesh <90479969+mohantyhitesh@users.noreply.github.com>
2021-10-24 21:40:13 -0700

    Update MovieRestController.java

--- src/main/java/com/hs/MovieService/rest/MovieRestController.java
+++ src/main/java/com/hs/MovieService/rest/MovieRestController.java
@@ -1,16 +1,23 @@
 package com.hs.MovieService.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.discovery.EurekaClient;
 
 @RestController
 @RequestMapping("/movie")
 public class MovieRestController {
 
+    @Autowired
+EurekaClient eurekaClient;
+	
 	 @GetMapping
-	    public Integer movie(){
-	        return Runtime.getRuntime().hashCode();
+	    public String movie(){
+	        return eurekaClient.getApplicationInfoManager().getInfo().getInstanceId();
 	    }
 	
 }
+
